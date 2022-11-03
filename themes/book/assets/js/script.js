e8c3c193b108f33b0f0d7effb9138855e6430be5
Abu Sakib <mabusakib@gmail.com>
2022-11-03 12:27:38 +0600

    Add custom JS to change website views within iframe.
    When the URL query parameter of `iframe` is `true`,
    
    - The website TOC is removed (but the page TOC remains).
    - The HubSpot customer chat widget is removed.

---
+++ themes/book/assets/js/script.js
@@ -0,0 +1,47 @@
+// Get header which contains the hamburger menus
+const header = document.querySelector("header");
+const menus = header.getElementsByTagName("label");
+
+// Grab the ZenID widget class from HubSpot
+
+// Using the mutation API to inspect the DOM for changes,
+// and in this case, for the HubSpot widget to load.
+function waitForHubSpot(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+// Get the query parameters from URL
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+
+// Get the value of `iframe` query  parameter
+const iframe_value = params.iframe;
+
+// Remove the HubSpot widget and left menu.
+// We only need to remove the left menu that shows our website TOC,
+// but we'll keep the page TOC.
+if (iframe_value) {
+  waitForHubSpot("#hubspot-messages-iframe-container").then(
+    (hubspot_widget) => {
+      console.log(hubspot_widget.remove());
+    }
+  );
+  menus[0].remove();
+}
