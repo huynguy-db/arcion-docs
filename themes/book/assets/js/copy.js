5f95c74c2f144babe814a9b4a3791c6fb2dcf820
Abu Sakib <sakib@arcion.io>
2023-08-17 11:45:38 +0600

    Add 'copy-to-clipboard' buttons for code samples

---
+++ themes/book/assets/js/copy.js
@@ -0,0 +1,36 @@
+// copy code icon markup
+const copyIcon = `<span class="material-symbols-outlined tooltip">content_copy</span>`
+const copySuccessIcon = `<span class="material-symbols-outlined">done</span>`
+
+// insert copy buttons for code blocks
+const codeBlocks = document.querySelectorAll("div.highlight")
+codeBlocks.forEach((codeBlock) => {
+  codeBlock.insertAdjacentHTML(
+    "afterbegin",
+    `<button class="copy" data-label="Click to copy" data-tooltip="Copy" aria-label="Copy code sample to clipboard">${copyIcon}</button>`
+  )
+})
+
+// handler that saves the code block innerText to clipboard
+function copyCodeBlock(event) {
+  const copyButton = event.currentTarget
+  copyButton.classList.add("no-hover")
+
+  const codeBlock = copyButton.parentElement.querySelector("div.highlight pre code")
+  const code = codeBlock.textContent.trim()
+  window.navigator.clipboard.writeText(code)
+
+  // change the button temporarily and restore  
+  // the hover effect and copy icon
+  copyButton.innerHTML = copySuccessIcon 
+  setTimeout(() => { 
+    copyButton.innerHTML = copyIcon;
+    copyButton.classList.remove("no-hover")
+  }, 3000)
+}
+
+// register event listeners for copy buttons
+const copyButtons = document.querySelectorAll("button.copy")
+copyButtons.forEach((button) => {
+  button.addEventListener("click", copyCodeBlock)
+})
